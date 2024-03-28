fd436af3dad8d5617163f05713c71f25fa1ff519
Luu Xuan Dat <75126436+lx-xuandat@users.noreply.github.com>
2024-03-28 23:20:41 +0700

    Create tailpress.sh

---
+++ tailpress.sh
@@ -0,0 +1,3 @@
+# downlaod package tailpress to folder global
+# run cmd via full path of package
+composer global require jeffreyvanrossum/tailpress-installer --ignore-platform-reqs && cd ~/Desktop && ~/.config/composer/vendor/bin/tailpress new theme_datlx --name="Luu Xuan Dat Theme" 
